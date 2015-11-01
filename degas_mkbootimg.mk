c2502fdd90dde8e47e0720613abcaf4945f08e21
Anonim47 <yosef@yosefadi.tk>
2015-11-01 20:22:05 +0700

    degas3g: remove derp

--- degas_mkbootimg.mk
+++ degas_mkbootimg.mk
@@ -12,7 +12,6 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): device/samsung/degas3g/tools/degas-mkbootimg 
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-        $(hide) 
 	$(hide) device/samsung/degas3g/tools/degas-mkbootimg $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
