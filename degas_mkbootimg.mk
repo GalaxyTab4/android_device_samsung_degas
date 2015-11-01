fa92f33bbbcc451defa876522d7bc32e0671f5ca
Anonim47 <yosef@yosefadi.tk>
2015-11-01 20:20:37 +0700

    degas3g: compile dt.img

--- degas_mkbootimg.mk
+++ degas_mkbootimg.mk
@@ -1,7 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+DTBTOOL := $(LOCAL_PATH)/tools/degas-dtbTool
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -p $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/dts/
+	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+
 $(INSTALLED_RECOVERYIMAGE_TARGET): device/samsung/degas3g/tools/degas-mkbootimg \
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
+        $(hide) 
 	$(hide) device/samsung/degas3g/tools/degas-mkbootimg $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
